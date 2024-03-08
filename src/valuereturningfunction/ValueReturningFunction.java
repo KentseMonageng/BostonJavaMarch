9905a9be231db592514f865ae68fda05cd9573ee
KMonagengAPMedia <kentse.monageng@apmedia.co.za>
2024-03-08 20:00:10 +0200

    first commit

---
+++ src/valuereturningfunction/ValueReturningFunction.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package valuereturningfunction;
+
+/**
+ *
+ * @author Future CEO
+ */
+public class ValueReturningFunction {
+
+    /**
+     * @param args the command line arguments
+     */
+    static int addition (int firstNum, int secondNum )
+    {
+        return firstNum + secondNum;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(addition(10,55));
+    }
+    
+}
